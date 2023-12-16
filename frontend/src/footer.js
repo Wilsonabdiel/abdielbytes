2b1276742aaf064e416638f3b55019f65296d41c
Wilsonabdiel <wilsonabdiel86@gmail.com>
2023-12-16 14:36:12 +0100

    feat:components

---
+++ frontend/src/footer.js
@@ -0,0 +1,14 @@
+const Footer = props => {
+    return (
+      React.createElement("footer", null, 
+      React.createElement("div", { className: "wrapper" }, 
+      React.createElement("h3", null, "THANKS FOR VISITING"), 
+      React.createElement("p", null, "\xA9 ", new Date().getFullYear(), " JaWiL."), 
+      React.createElement(SocialLinks, null))));
+  
+  
+  
+  };
+  
+  
+  
--- frontend/src/home.js
+++ frontend/src/home.js
@@ -1,5 +1,13 @@
-const  = () => {
-    return (  );
+const Home = () => {
+
+
+    return ( 
+        <div>
+            <h1>Abdiel John WIlson</h1>
+            <p>Hi humans and extraterritrials. I am Abdiel J. Wilson a Fullstack Software Engineer </p>
+        
+        </div>
+     );
 }
  
-export default ;
+export default Home;
