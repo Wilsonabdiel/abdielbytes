953c1ef74834fc1336dfa597139455945d1194a3
Wilsonabdiel <wilsonabdiel86@gmail.com>
2023-12-17 22:13:53 +0100

    feat:index

---
+++ abdiel/src/Nav.js
@@ -0,0 +1,9 @@
+const Nav = () => {
+    return ( 
+        <section className="">
+            
+        </section>
+     );
+}
+ 
+export default Nav;
---
+++ abdiel/src/home.js
@@ -0,0 +1,34 @@
+const Home = () => {
+    return ( 
+        <section className="index">
+            <div className="main">
+                <h5>
+                    Hey there human, I'm a full stack developer and a fanatical open sourcer in the making.
+                </h5>
+                <ul>
+                    {/* To be made responsive. */}
+                    <li>I worked at SoftSuite</li>
+                    <li>Creator of  BCH eResources </li>
+                    <li>Team member at Uniuyo Backend</li>
+                </ul>
+                <p>I am a software engineer and a writer well versed in the inticasies of the backend and the frontend.</p>
+                <p>
+
+                        I enjoy problem-solving and creating visually appealing and user-friendly interfaces, and well perfroming, scallable and secure backends. My passion for learning is insatiable—I thrive on acquiring new knowledge and skills. Writing has always held a special place in my heart; it's a hobby I've cherished since high school. I'm not just a coder; I'm also a poet, expressing my thoughts through words. Additionally, I have a deep passion for food, savoring both the culinary art and its cultural significance!
+                </p>
+                <div>
+                    <h6>What I DO...</h6>
+             
+                    <p>
+                    Front-End: I wield JavaScript (solo or with React/Vue) to bring web apps to life. For design, I mix Sass & CSS, adding Bootstrap/Bulma when needed.
+                    </p> <p>
+                    Back-End: PHP (solo or frameworks like Laravel/Symphony) and Python (Django/Flask) are my weapons of choice. I adapt to project needs, even wielding WordPress when the situation demands it.</p>
+                    <p>Words my playground, pixels my escape. I weave intricate tapestries of verse, emotions dancing on paper. Then, I dive into worlds teeming with challenge, wielding wit and strategy like enchanted weapons. In every line, every quest, I seek the spark of the extraordinary</p>
+                </div>
+            </div>
+            
+        </section>
+        );
+}
+ 
+export default Home;
