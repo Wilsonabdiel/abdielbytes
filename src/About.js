d8366f0921676f3f02908416c01c4f6fd9b78b36
Wilsonabdiel <wilsonabdiel86@gmail.com>
2024-02-18 15:14:39 +0100

    font swtich

--- src/About.js
+++ src/About.js
@@ -20,23 +20,26 @@ const About = () => (
                     <p className="separator" />
 
                     <div className="desc full">
-                        <h4 className="subtitle">My name is Abdiel. </h4>
-                        <p>I am a web developer and a writer well versed
-                            in the beauty of the backend with knowlegde of the frontend.</p>
+                        <h4 className="subtitle">Hi Humans, I'm Abdiel. </h4>
+                        <p>A web developer and a writer based south of the milky way.</p>
                         <p>
-                            I enjoy problem-solving and creating visually
-                            appealing and user-friendly interfaces,
-                            and well perfroming, scallable and secure backends.
+                            I enjoy a few things, problem solving, creating well performing, secure, scallable and visually appealing applications
+                           </p>
+                           <p>
                             My passion for learning is insatiable. I thrive on acquiring new
-                            knowledge and skills. Writing has always held a
+                            knowledge and skills. </p>
+                            <p>
+                                And oh writing! Writing has always held a
                             special place in my heart, it is a hobby
-                            I have cherished since high school, where my
-                            teachers praised my work. I am not just a coder,
-                            I am also a poet, expressing my thoughts through words.
-                            Additionally, I have a deep passion for food,
-                            savoring both the culinary art and its cultural significance!
+                            I have enjoyed for a very long time now. I am not just an engineer
+                            I am also a poet, expressing my thoughts through the rhythmn of words. </p>
+                            <p>
+                            Also, I love food, in a healthy way of course.
+                            I enjoy
+                            both the culinary art and its cultural significance!
 
                         </p>
+                        <p><strong>That's a little about me.</strong></p>
                     </div>
                 </div>
 
@@ -44,20 +47,13 @@ const About = () => (
                     <h3>What does he do?</h3>
                     <p className="separator" /></div>
                 <div className="desc">
-                    <h4 className="subtitle">I am a programmer.</h4>
-                    <p>In my front-end work, I typically utilize
-                        JavaScript, both independently and in conjunction
-                        with well-known frameworks such as ReactJS
-                        and VueJS. Additionally, I enhance the
-                        aesthetics of web applications using
-                        Sass, CSS, and, when necessary, complementary
-                        tools like Bootstrap and Bulma.</p>
-                    <p>For the backend I work with PHP,
+                    <h4 className="subtitle">I am a <a href='#portfolio'>programmer</a>.</h4>
+                   
+                    <p>I work with PHP,
                         either standalone or with its frameworks
                         like laravel and symphony. I also work with
-                        Python and its frameworks Django and Flask.
-                        I work with whatever the project requires
-                        even Wordpress when situations require it </p>
+                        Python and its frameworks Django and Flask. I have built several projects in the forementioned languages
+                     </p>
 
 
                 </div>
@@ -71,7 +67,7 @@ const About = () => (
                         enabling me to convey my thoughts in distinctive and captivating ways.
                         Beyond the written word, I also find solace and excitement in the virtual
                         landscapes of video games,
-                        where I embark on thrilling adventures and put my strategic thinking to the test.</p> *
+                        where I embark on thrilling adventures and put my strategic thinking to the test.</p> 
                 </div>
             </article>
         </div>
