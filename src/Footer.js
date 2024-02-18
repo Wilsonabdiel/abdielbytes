7a503c7654be7286a0f67e3f2a77744e90da2912
Wilsonabdiel <wilsonabdiel86@gmail.com>
2024-02-18 15:40:00 +0100

    footer

--- src/Footer.js
+++ src/Footer.js
@@ -5,8 +5,7 @@ import './static/Footer.css';
 const Footer = () => (
     <footer>
         <div className="wrapper">
-            {/* <h2 className="title">THANKS FOR VISITING</h2> */}
-            <p className="copyright">Copyright &copy; {new Date().getFullYear()} JaWiL.</p>
+                  <p className="copyright">Copyright &copy; {new Date().getFullYear()} JaWiL.</p>
             <SocialLinks />
         </div>
     </footer>
